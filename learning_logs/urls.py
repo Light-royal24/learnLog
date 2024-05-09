5aa6ab8bd0214090b88e60808750ab1b7f95136e
Light-royal24 <royallight24@gmail.com>
2024-05-08 18:49:46 -0700

    first commit

---
+++ learning_logs/urls.py
@@ -0,0 +1,22 @@
+"""Define URL patterns for learning_logs"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    # page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+    # Details page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # page for adding new topics
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Page for editing an entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+]
